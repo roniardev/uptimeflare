2dffebe332ad2b9b118bb529e1e97a006e41ad66
Roni Ardiyanto <35429278+roniardev@users.noreply.github.com>
2024-11-21 14:21:12 +0700

    fix(config): uptime config

--- uptime.config.ts
+++ uptime.config.ts
@@ -10,13 +10,8 @@ const pageConfig = {
 }
 
 const workerConfig = {
-  // Write KV at most every 3 minutes unless the status changed
   kvWriteCooldownMinutes: 3,
-  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
-  // passwordProtection: 'username:password',
-  // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
       // `id` should be unique, history will be kept if the `id` remains constant
       id: 'personal_web_monitor',
@@ -31,49 +26,16 @@ const workerConfig = {
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
       statusPageLink: 'https://roniar.dev'
       // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
+      expectedCodes: [200]
       // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
       timeout: 10000,
       // [OPTIONAL] headers to be sent
       headers: {
         'User-Agent': 'Uptimeflare',
         // Authorization: 'Bearer YOUR_TOKEN_HERE',
-      },
-      // [OPTIONAL] body to be sent
-      // body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      // responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      // checkLocationWorkerRoute: 'https://xxx.example.com',
-    },
-    // Example TCP Monitor
-    // {
-    //   id: 'test_tcp_monitor',
-    //   name: 'Example TCP Monitor',
-    //   // `method` should be `TCP_PING` for tcp monitors
-    //   method: 'TCP_PING',
-    //   // `target` should be `host:port` for tcp monitors
-    //   target: '1.2.3.4:22',
-    //   tooltip: 'My production server SSH',
-    //   statusPageLink: 'https://example.com',
-    //   timeout: 5000,
-    // },
+      }
+    }
   ],
-  // notification: {
-  //   // [Optional] apprise API server URL
-  //   // if not specified, no notification will be sent
-  //   // appriseApiServer: "https://apprise.example.com/notify",
-  //   // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
-  //   // if not specified, no notification will be sent
-  //   // recipientUrl: "tgram://bottoken/ChatID",
-  //   // [Optional] timezone used in notification messages, default to "Etc/GMT"
-  //   timeZone: "Asia/Shanghai",
-  //   // [Optional] grace period in minutes before sending a notification
-  //   // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-  //   // if not specified, notification will be sent immediately
-  //   gracePeriod: 5,
-  // },
   callbacks: {
     onStatusChange: async (
       env: any,
